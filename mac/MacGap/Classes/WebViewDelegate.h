d8e722dbf62348d9dff8cffcfb6d07dfec418595
Tatsuya Miyamae <miyamae@bitarts.co.jp>
2012-05-31 19:28:21 +0900

    new file

---
+++ mac/MacGap/Classes/WebViewDelegate.h
@@ -0,0 +1,33 @@
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+@class Sound;
+@class Dock;
+@class Growl;
+@class Path;
+@class App;
+@class Window;
+
+@class WindowController;
+
+@interface WebViewDelegate : NSObject {
+	Sound* sound;
+    Dock* dock;
+    Growl* growl;
+    Path* path;
+    App* app;
+    Window* window;
+}
+
+
+
+@property (nonatomic, retain) Sound* sound;
+@property (nonatomic, retain) Dock* dock;
+@property (nonatomic, retain) Growl* growl;
+@property (nonatomic, retain) Path* path;
+@property (nonatomic, retain) App* app;
+@property (nonatomic, retain) Window* window;
+
+@property (nonatomic, retain) WindowController *requestedWindow;
+
+@end
